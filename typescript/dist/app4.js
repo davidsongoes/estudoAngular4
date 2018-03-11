880d3c134417c5c6d94d6ee6356575740cfcb96b
Davidson Góes <davidsongoes@gmail.com>
2018-03-10 21:37:01 -0300

    Arquivos compilados dos ts

--- typescript/dist/app4.js
+++ typescript/dist/app4.js
@@ -3,28 +3,34 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 }
 Object.defineProperty(exports, "__esModule", { value: true });
-var ConcessionariaDao_1 = require("./ConcessionariaDao");
+// import {ConcessionariaDao} from './ConcessionariaDao'
 var Concessionaria_1 = __importDefault(require("./Concessionaria"));
-var PessoaDao_1 = require("./PessoaDao");
+// import { PessoaDao } from './PessoaDao';
 var Pessoa_1 = __importDefault(require("./Pessoa"));
-var CarroDao_1 = require("./CarroDao");
+// import { CarroDao } from './CarroDao'
 var Carro_1 = __importDefault(require("./Carro"));
-var MotoDao_1 = require("./MotoDao");
+// import { MotoDao } from './MotoDao'
 var Moto_1 = __importDefault(require("./Moto"));
-var VeiculoDao_1 = require("./VeiculoDao");
+// import { VeiculoDao } from './VeiculoDao'
 var Veiculo_1 = __importDefault(require("./Veiculo"));
-var dao = new ConcessionariaDao_1.ConcessionariaDao;
+// let dao: ConcessionariaDao = new ConcessionariaDao
 var concessionaria = new Concessionaria_1.default('', []);
-dao.inserir(concessionaria);
-var dao2 = new PessoaDao_1.PessoaDao();
+// let dao2: PessoaDao = new PessoaDao()
 var pessoa = new Pessoa_1.default('', '');
-dao2.atualizar(pessoa);
-var dao3 = new CarroDao_1.CarroDao();
+// let dao3: CarroDao = new CarroDao()
 var carro = new Carro_1.default('', 0);
-dao3.inserir(carro);
-var dao4 = new MotoDao_1.MotoDao();
+// let dao4: MotoDao = new MotoDao()
 var moto = new Moto_1.default();
-dao4.atualizar(moto);
-var dao5 = new VeiculoDao_1.VeiculoDao();
+// let dao5: VeiculoDao = new VeiculoDao()
 var veiculo = new Veiculo_1.default();
-dao5.inserir(veiculo);
+var Dao_1 = require("./Dao");
+var dao6 = new Dao_1.Dao();
+var dao7 = new Dao_1.Dao();
+var dao8 = new Dao_1.Dao();
+var dao9 = new Dao_1.Dao();
+var dao10 = new Dao_1.Dao();
+dao6.atualizar(concessionaria);
+dao7.inserir(pessoa);
+dao8.atualizar(carro);
+dao9.inserir(moto);
+dao10.atualizar(veiculo);

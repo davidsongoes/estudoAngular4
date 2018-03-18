106b4194953dd374ac9dd7ccc33e81f1ac0975c1
Davidson Góes <davidsongoes@gmail.com>
2018-03-18 11:32:16 -0300

    Implementando lógica do progresso

--- projetos/app1/src/app/painel/painel.component.ts
+++ projetos/app1/src/app/painel/painel.component.ts
@@ -17,32 +17,39 @@ export class PainelComponent implements OnInit {
   public rodada: number = 0
   public rodadaFrase: Frase
 
-  constructor() { 
+  public progresso: number = 0
+
+  constructor() {
     this.rodadaFrase = this.frases[this.rodada]
     console.log(this.rodadaFrase)
-   }
+  }
 
   ngOnInit() {
   }
 
-  public atualizaResposta(resposta: Event): void{
+  public atualizaResposta(resposta: Event): void {
     this.resposta = ((<HTMLInputElement>resposta.target).value)
     // console.log(this.resposta)
   }
 
-  public verificarResposta(): void{
+  public verificarResposta(): void {
 
-    if(this.rodadaFrase.frasePtBr == this.resposta){
-      alert( 'A traduação está correta' )
-       // Trocar pergunta da rodada
-       this.rodada++
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      alert('A traduação está correta')
+      // Trocar pergunta da rodada
+      this.rodada++
+
+
+      // Progresso
+      this.progresso = this.progresso + (100 / this.frases.length)
+console.log(this.progresso)
 
       //  Atualiza o objeto rodadaFrase
-       this.rodadaFrase = this.frases[this.rodada]
-    } else{
-      alert( 'Atradução está errada' )
+      this.rodadaFrase = this.frases[this.rodada]
+    } else {
+      alert('Atradução está errada')
     }
-     
+
   }
 
 }

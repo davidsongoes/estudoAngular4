af3e26c4b4cb16f2137d033d40b5f23ab89904f8
davidsongoes <davidsongoes@gmail.com>
2018-04-01 16:28:19 -0300

    Retirando consoles log

--- projetos/app1/src/app/tentativas/tentativas.component.ts
+++ projetos/app1/src/app/tentativas/tentativas.component.ts
@@ -16,7 +16,6 @@ public coracoes: Coracao[] = [
 ]
 
   constructor() {
-    console.log(this.coracoes)
    }
 
    ngOnChanges(){
@@ -26,7 +25,6 @@ public coracoes: Coracao[] = [
       let indice = this.coracoes.length - this.tentativas
       this.coracoes[indice - 1].cheio = false
     }
-    console.log('Tentativas restantes do painel: ', this.tentativas)
   }
 
   ngOnInit() {

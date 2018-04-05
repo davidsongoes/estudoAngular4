e6624f2d65145757e9ce0e1de47c0c7aeb71fb21
Davidson Góes <davidsongoes@gmail.com>
2018-04-05 15:02:54 -0300

    Refatorando url da api

--- projetos/app2/src/app/ofertas.service.ts
+++ projetos/app2/src/app/ofertas.service.ts
@@ -6,23 +6,25 @@ import 'rxjs/add/operator/toPromise'
 @Injectable()
 export class OfertasService {
 
+    private urlApi = 'http://localhost:3000/ofertas'
+
     constructor(private http: Http) { }
 
     public getOfertas(): Promise<Array<Oferta>> {
         // Efetuar uma requisição http 
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get(`${this.urlApi}?destaque=true`)
             .toPromise()
             .then((response: any) => response.json())
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Array<Oferta>> {
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${this.urlApi}?categoria=${categoria}`)
         .toPromise()
         .then((response: any) => response.json())
     }
 
     public getOfertaPorId(id: number): Promise<Oferta>{
-       return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+       return this.http.get(`${this.urlApi}?id=${id}`)
        .toPromise()
        .then((response: any) => {
             return response.json()[0]

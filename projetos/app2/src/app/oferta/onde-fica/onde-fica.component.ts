cb9fb1b08f63be06ed88b51884e25254b899bc14
Davidson Góes <davidsongoes@gmail.com>
2018-04-05 16:35:25 -0300

    Implementando rota

--- projetos/app2/src/app/oferta/onde-fica/onde-fica.component.ts
+++ projetos/app2/src/app/oferta/onde-fica/onde-fica.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+ 
 @Component({
   selector: 'app-onde-fica',
   templateUrl: './onde-fica.component.html',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OndeFicaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log('ID da rota Pai', this.route.parent.snapshot.params['id'])
   }
 
 }

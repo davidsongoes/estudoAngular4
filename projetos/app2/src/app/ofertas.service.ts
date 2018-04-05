8202f2104d5a4734e65d74e436e2b984d035513c
Davidson Góes <davidsongoes@gmail.com>
2018-04-05 09:47:39 -0300

    Implementando método getOfertasPorCategoria

--- projetos/app2/src/app/ofertas.service.ts
+++ projetos/app2/src/app/ofertas.service.ts
@@ -1,4 +1,4 @@
-import { Http} from '@angular/http'
+import { Http } from '@angular/http'
 import { Injectable } from '@angular/core'
 import { Oferta } from './shared/oferta.model'
 import 'rxjs/add/operator/toPromise'
@@ -6,11 +6,17 @@ import 'rxjs/add/operator/toPromise'
 @Injectable()
 export class OfertasService {
 
-    constructor(private http: Http){}
+    constructor(private http: Http) { }
 
     public getOfertas(): Promise<Array<Oferta>> {
         // Efetuar uma requisição http 
         return this.http.get('http://localhost:3000/ofertas?destaque=true')
+            .toPromise()
+            .then((response: any) => response.json())
+    }
+
+    public getOfertasPorCategoria(categoria: string): Promise<Array<Oferta>> {
+        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
         .toPromise()
         .then((response: any) => response.json())
     }

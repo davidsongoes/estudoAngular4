6f24034585880d935145efb7825f85991406ad66
Davidson Góes <davidsongoes@gmail.com>
2018-04-05 15:20:46 -0300

    Refatorando url da api no serviço por constante

--- projetos/app2/src/app/ofertas.service.ts
+++ projetos/app2/src/app/ofertas.service.ts
@@ -1,30 +1,29 @@
 import { Http } from '@angular/http'
 import { Injectable } from '@angular/core'
 import { Oferta } from './shared/oferta.model'
+import { URL_API } from './app.api'
 import 'rxjs/add/operator/toPromise'
 
 @Injectable()
 export class OfertasService {
 
-    private urlApi = 'http://localhost:3000/ofertas'
-
     constructor(private http: Http) { }
 
     public getOfertas(): Promise<Array<Oferta>> {
         // Efetuar uma requisição http 
-        return this.http.get(`${this.urlApi}?destaque=true`)
+        return this.http.get(`${URL_API}?destaque=true`)
             .toPromise()
             .then((response: any) => response.json())
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Array<Oferta>> {
-        return this.http.get(`${this.urlApi}?categoria=${categoria}`)
+        return this.http.get(`${URL_API}?categoria=${categoria}`)
         .toPromise()
         .then((response: any) => response.json())
     }
 
     public getOfertaPorId(id: number): Promise<Oferta>{
-       return this.http.get(`${this.urlApi}?id=${id}`)
+       return this.http.get(`${URL_API}?id=${id}`)
        .toPromise()
        .then((response: any) => {
             return response.json()[0]

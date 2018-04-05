8123e98908097f8c752db94fd80a5056af59432e
Davidson Góes <davidsongoes@gmail.com>
2018-04-05 14:28:10 -0300

    Comentando console.log

--- projetos/app2/src/app/oferta/oferta.component.ts
+++ projetos/app2/src/app/oferta/oferta.component.ts
@@ -24,7 +24,7 @@ export class OfertaComponent implements OnInit {
    this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
    .then(( oferta: Oferta ) => {
       this.oferta = oferta
-      console.log(this.oferta)
+      // console.log(this.oferta)
    })
   }
 

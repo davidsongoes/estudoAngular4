66e62902cf685507f0da3aa9bd355736903a8bd7
davidsongoes <davidsongoes@gmail.com>
2018-04-24 21:55:03 -0300

    Subindo updates do app2

--- projetos/app2/src/app/oferta/onde-fica/onde-fica.component.ts
+++ projetos/app2/src/app/oferta/onde-fica/onde-fica.component.ts
@@ -11,7 +11,7 @@ export class OndeFicaComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    console.log('ID da rota Pai', this.route.parent.snapshot.params['id'])
+    console.log('ID da rota Pai - Onde fica', this.route.parent.snapshot.params['id'])
   }
 
 }
--- projetos/app2/src/app/ofertas.service.ts
+++ projetos/app2/src/app/ofertas.service.ts
@@ -11,22 +11,30 @@ export class OfertasService {
 
     public getOfertas(): Promise<Array<Oferta>> {
         // Efetuar uma requisição http 
-        return this.http.get(`${URL_API}?destaque=true`)
+        return this.http.get(`${URL_API}/ofertas?destaque=true`)
             .toPromise()
             .then((response: any) => response.json())
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Array<Oferta>> {
-        return this.http.get(`${URL_API}?categoria=${categoria}`)
+        return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
         .toPromise()
         .then((response: any) => response.json())
     }
 
     public getOfertaPorId(id: number): Promise<Oferta>{
-       return this.http.get(`${URL_API}?id=${id}`)
+       return this.http.get(`${URL_API}/ofertas?id=${id}`)
        .toPromise()
        .then((response: any) => {
             return response.json()[0]
        })
+    }
+
+    public getComoUsarOfertaPorId(id: number): Promise<string>{
+        return this.http.get(`${URL_API}/como-usar?id=${id}`)
+        .toPromise()
+        .then((response: any) => {
+            return response.json()[0].descricao
+        })
     }
 }

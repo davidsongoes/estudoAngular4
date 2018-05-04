0f18d2de3540ffaf20cc196839af81c21b62850f
Davidson Góes <davidsongoes@gmail.com>
2018-05-04 16:41:52 -0300

    Subindo aula de Observable

--- projetos/app2/src/app/oferta/oferta.component.ts
+++ projetos/app2/src/app/oferta/oferta.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import 'rxjs/Rx'
+import { interval } from 'rxjs/observable/interval';
 
 @Component({
   selector: 'app-oferta',
@@ -12,20 +16,44 @@ import { Oferta } from '../shared/oferta.model';
   ]
 })
 export class OfertaComponent implements OnInit {
-  
+
   public oferta: Oferta
 
   constructor(
     private ofertasService: OfertasService,
     private route: ActivatedRoute
-  ) {}
+  ) { }
 
   ngOnInit() {
-   this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
-   .then(( oferta: Oferta ) => {
-      this.oferta = oferta
-      // console.log(this.oferta)
-   })
-  }
+    this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
+      .then((oferta: Oferta) => {
+        this.oferta = oferta
+        // console.log(this.oferta)
+      })
 
+    //  this.route.params.subscribe((parametro: any) => {console.log(parametro)},
+    //   (erro: any) => console.log(erro),
+    //   () => console.log('Processamento foi classificado como concluído!')
+    //   )
+
+    // let tempo = Observable.interval(2000)
+
+    // tempo.subscribe((intervalo: number) => {
+    //   console.log(intervalo)
+    // })
+
+    // Observable(Observável)
+    let meuObservableTeste = Observable.create((observer: Observer<number>) => {
+      observer.next(1)
+      observer.next(3)
+      observer.complete()
+      observer.error('Algum erro foi encotrado na stream de eventos')
+    })
+    // Observable(Observador)
+    meuObservableTeste.subscribe(
+      (response: number) => console.log(response + 10),
+      (error: string) => console.log(error) ,
+      () => console.log('Stream de eventos foi finalizada')
+    )
+  }
 }

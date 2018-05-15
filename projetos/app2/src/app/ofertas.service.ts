45450ee39412b34e6173a58904f1971a21b18796
Davidson Góes <davidsongoes@gmail.com>
2018-05-15 15:20:10 -0300

    Aula final sobre Observables

--- projetos/app2/src/app/ofertas.service.ts
+++ projetos/app2/src/app/ofertas.service.ts
@@ -1,9 +1,10 @@
-import { Http } from '@angular/http'
+import { Http, Response } from '@angular/http'
 import { Injectable } from '@angular/core'
 import { Oferta } from './shared/oferta.model'
 import { URL_API } from './app.api'
 import 'rxjs/add/operator/toPromise'
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/retry'
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
@@ -15,19 +16,19 @@ export class OfertasService {
         // Efetuar uma requisição http 
         return this.http.get(`${URL_API}/ofertas?destaque=true`)
             .toPromise()
-            .then((response: any) => response.json())
+            .then((response: Response) => response.json())
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Array<Oferta>> {
         return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
         .toPromise()
-        .then((response: any) => response.json())
+        .then((response: Response) => response.json())
     }
 
     public getOfertaPorId(id: number): Promise<Oferta>{
        return this.http.get(`${URL_API}/ofertas?id=${id}`)
        .toPromise()
-       .then((response: any) => {
+       .then((response: Response) => {
             return response.json()[0]
        })
     }
@@ -35,7 +36,7 @@ export class OfertasService {
     public getComoUsarOfertaPorId(id: number): Promise<string>{
         return this.http.get(`${URL_API}/como-usar?id=${id}`)
         .toPromise()
-        .then((response: any) => {
+        .then((response: Response) => {
             return response.json()[0].descricao
         })
     }
@@ -43,13 +44,14 @@ export class OfertasService {
     public getOndeFicarOfertaPorId(id: number): Promise<string>{
         return this.http.get(`${URL_API}/onde-fica?id=${id}`)
         .toPromise()
-        .then((response: any) => {
+        .then((response: Response) => {
             return response.json()[0].descricao
         })
     }
 
     public pesquisaOFertas(termo: string) : Observable<Oferta[]>{
-        return this.http.get(`${URL_API}/ofertas?descricao_oferta=${termo}`)
-        .map((response: any) => response.json())
+        return this.http.get(`${URL_API}/ofertas?descricao_oferta_like=${termo}`)
+        .retry(10)
+        .map((response: Response) => response.json())
     }
 }
--- projetos/app2/src/app/topo/topo.component.ts
+++ projetos/app2/src/app/topo/topo.component.ts
@@ -1,23 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service'
+import { Observer } from 'rxjs/Observer';
+import { Observable } from 'rxjs/Observable';
+import { Oferta } from '../shared/oferta.model'
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/observable/of'
+
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
   styleUrls: ['./topo.component.css'],
-  providers: [ OfertasService ]
+  providers: [OfertasService]
 })
 export class TopoComponent implements OnInit {
 
+  public ofertas: Observable<Oferta[]>
+  public ofertas2: Oferta[]
+  private subjectPesquisa: Subject<string> = new Subject<string>()
   constructor(
     private ofertasService: OfertasService
   ) { }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa
+    .debounceTime(1000)
+    .distinctUntilChanged()
+    .switchMap((termo: string) => {
+      console.log('Requisição http para api')
+      if(termo.trim() === ''){
+        return Observable.of<Oferta[]>([])
+      }
+      return this.ofertasService.pesquisaOFertas(termo)
+    })
+    .catch((err: any) => {
+      console.log(err)
+      return Observable.of<Oferta[]>([])
+    })
+    this.ofertas.subscribe((ofertas: Oferta[]) => this.ofertas2 = ofertas)
   }
 
-  public pesquisa(termoDaBusca:string) : void {
-    console.log(termoDaBusca)
+  public pesquisa(termoDaBusca: string): void {
+    console.log('KeyUp caracter: ', termoDaBusca)
+    this.subjectPesquisa.next(termoDaBusca)
   }
-
 }

9c90efccf6dbf84f0b29129c501d45e3c36a6bd2
davidsongoes <davidsongoes@gmail.com>
2018-05-25 19:23:40 -0300

    Implementando pipe customizado

--- projetos/app2/src/app/app.module.ts
+++ projetos/app2/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { DiversaoComponent } from './diversao/diversao.component';
 import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
+import { DescricaoReduzida } from './util/descricao-reduzida.pipe'
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
     DiversaoComponent,
     OfertaComponent,
     ComoUsarComponent,
-    OndeFicaComponent
+    OndeFicaComponent,
+    DescricaoReduzida
   ],
   imports: [
     BrowserModule,

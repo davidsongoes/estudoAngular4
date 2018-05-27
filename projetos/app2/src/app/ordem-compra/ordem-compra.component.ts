c82864e5feb63296899be53f0e994a5bfc98d083
davidsongoes <davidsongoes@gmail.com>
2018-05-27 19:30:22 -0300

    Implementando atributo controle dos estados primitivos dos elementos

--- projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
+++ projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -18,6 +18,12 @@ export class OrdemCompraComponent implements OnInit {
   public numeroValido: boolean
   public formaPagamentoValido: boolean
 
+  // Atributos estados primitivos dos campos (pristine state)
+  public enderecoEstadoPrimitivo: boolean = true
+  public numeroEstadoPrimitivo: boolean = true
+  public complementoEstadoPrimitivo: boolean = true
+  public formaPagamentoEstadoPrimitivo: boolean = true
+  
   constructor() { }
 
   ngOnInit() {
@@ -25,6 +31,8 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaEndereco(endereco: string): void {
     this.endereco = endereco
+    this.enderecoEstadoPrimitivo = false
+
     // console.log(this.endereco)
     if (this.endereco.length > 3) {
       this.enderecoValido = true
@@ -35,6 +43,7 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaNumero(numero: string): void {
     this.numero = numero
+    this.numeroEstadoPrimitivo = false
     // console.log(this.numero)
     if (this.numero.length > 0) {
       this.numeroValido = true
@@ -45,6 +54,7 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaComplemento(complemento: string): void {
     this.complemento = complemento
+    this.complementoEstadoPrimitivo = false
     // console.log(this.complemento)
     if (this.complemento.length > 0) {
       this.complementoValido = true
@@ -53,7 +63,8 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaFormaPagamento(formaPagamento: string): void {
     this.formaPagamento = formaPagamento
-    console.log(this.formaPagamento)
+    this.formaPagamentoEstadoPrimitivo = false
+    // console.log(this.formaPagamento)
     if (this.formaPagamento.length > 0) {
       this.formaPagamentoValido = true
     } else {

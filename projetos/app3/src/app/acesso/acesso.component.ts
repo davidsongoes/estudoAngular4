3a5cebeef51b22103583aef9bd90e688c5dd9455
davidsongoes <davidsongoes@gmail.com>
2018-06-02 21:00:13 -0300

    implementando animação do painel

--- projetos/app3/src/app/acesso/acesso.component.ts
+++ projetos/app3/src/app/acesso/acesso.component.ts
@@ -17,12 +17,25 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
        }),
        animate('500ms 0s ease-in-out')
      ])
+    ]),
+    trigger('animacao-painel', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({
+          opacity: 0,
+          transform: 'translate(30px, 0)'
+        }),
+        animate('500ms 0s ease-in-out')
+      ])
     ])
   ]
 })
 export class AcessoComponent implements OnInit {
 
   public estadoBanner: string = 'criado'
+  public estadoPainel: string = 'criado'
 
   constructor() { }
 

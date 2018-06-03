478d8755c7e2cf9fb4106d608db93246602cd328
davidsongoes <davidsongoes@gmail.com>
2018-06-03 14:38:54 -0300

    Implementando keyframes na animação do painel

--- projetos/app3/src/app/acesso/acesso.component.ts
+++ projetos/app3/src/app/acesso/acesso.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
@@ -27,7 +27,15 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
           opacity: 0,
           transform: 'translate(30px, 0)'
         }),
-        animate('500ms 0s ease-in-out')
+        animate('500ms 0s ease-in-out', keyframes([
+          style({offset: 0.45, opacity:1, transform: 'translateX(0)'}),
+          style({offset: 0.80, opacity:1, transform: 'translateX(0)'}),
+          style({offset: 0.84, opacity:1, transform: 'translateY(-10px)'}),
+          style({offset: 0.88, opacity:1, transform: 'translateY(10px)'}),
+          style({offset: 0.92, opacity:1, transform: 'translateY(-10px)'}),
+          style({offset: 0.96, opacity:1, transform: 'translateY(10px)'}),
+          style({offset: 1, opacity:1, transform: 'translateX(0)'})
+        ]))
       ])
     ])
   ]

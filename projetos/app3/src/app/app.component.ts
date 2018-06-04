97dbede5e5d046545697f5ab45d98d8e27fbfca6
goesdgbp <davidsongoes@gmail.com>
2018-06-04 10:59:47 -0300

    Importando recursos do Firebase no app.component e configurando conexões

--- projetos/app3/src/app/app.component.ts
+++ projetos/app3/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  public configFirebase = {
+    apiKey: "AIzaSyCGiqHUegVErgu9gSJVpZEgs0jYPb1a_10",
+    authDomain: "jta-instagram-clone-86f91.firebaseapp.com",
+    databaseURL: "https://jta-instagram-clone-86f91.firebaseio.com",
+    projectId: "jta-instagram-clone-86f91",
+    storageBucket: "jta-instagram-clone-86f91.appspot.com",
+    messagingSenderId: "679543468650"
+  }
+
+  ngOnInit(): void {
+    firebase.initializeApp(this.configFirebase)
+  }
 }

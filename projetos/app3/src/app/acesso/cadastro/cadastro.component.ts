112c4015604ddaabeaf3100aa39e28695b007209
goesdgbp <davidsongoes@gmail.com>
2018-06-04 10:16:57 -0300

    Implementando ReactiveForms no cadastro

--- projetos/app3/src/app/acesso/cadastro/cadastro.component.ts
+++ projetos/app3/src/app/acesso/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormControl, Validator, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-cadastro',
@@ -9,6 +10,14 @@ export class CadastroComponent implements OnInit {
 
   @Output() public exibirPainel: EventEmitter<string> = new EventEmitter<string>()
 
+  public formulario: FormGroup = new FormGroup({
+    'email': new FormControl(null, [Validators.required]),
+    'nome_completo': new FormControl(null, [Validators.required]),
+    'nome_usuario': new FormControl(null, [Validators.required]),
+    'senha': new FormControl(null, [Validators.required])
+
+  })
+
   constructor() { }
 
   ngOnInit() {
@@ -18,4 +27,7 @@ export class CadastroComponent implements OnInit {
     this.exibirPainel.emit('login')
   }
 
+  public cadastrarUsuario(): void {
+    console.log(this.formulario)
+  }
 }

2ed4e7079280ffaed9181379ffd2f1fefcb8e677
goesdgbp <davidsongoes@gmail.com>
2018-06-04 13:40:33 -0300

    Implementando persistencia dos dados no database do firebase

--- projetos/app3/src/app/autenticacao.service.ts
+++ projetos/app3/src/app/autenticacao.service.ts
@@ -6,6 +6,15 @@ export class AutenticacaoService {
         firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(usuario.email, usuario.senha)
             .then((response) => {
                 console.log(response)
+
+                // remover a senha do atributo senha do objeto usuário
+                delete usuario.senha
+
+                // registrando dados complementares no path email na base 64
+                firebase.database().ref(`usuario_detalhe/${btoa(usuario.email)}`)
+                .set(usuario)
+                console.log(usuario)
+
             })
             .catch((error: Error) => {
                 console.log(error)

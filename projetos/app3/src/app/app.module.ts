556eef8b6f1736680972e6e43c2c375aa0c15de8
goesdgbp <davidsongoes@gmail.com>
2018-06-04 17:07:02 -0300

    Implementando as rotas

--- projetos/app3/src/app/app.module.ts
+++ projetos/app3/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { ReactiveFormsModule } from '@angular/forms'
 
+import { RouterModule} from '@angular/router'
+import { ROUTES } from './app.routes'
+
 import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
 import { BannerComponent } from './acesso/banner/banner.component';
@@ -26,7 +29,8 @@ import { PublicacoesComponent } from './home/publicacoes/publicacoes.component'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [AutenticacaoService],
   bootstrap: [AppComponent]

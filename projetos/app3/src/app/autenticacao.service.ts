05c3491b347d5270966007ea17633c4d129c6ae0
goesdgbp <davidsongoes@gmail.com>
2018-06-04 17:24:29 -0300

    Refatorando método de void para Promise

--- projetos/app3/src/app/autenticacao.service.ts
+++ projetos/app3/src/app/autenticacao.service.ts
@@ -2,8 +2,8 @@ import { Usuario } from "./acesso/usuario.model";
 import * as firebase from 'firebase'
 
 export class AutenticacaoService {
-    public cadastrarUsuario(usuario: Usuario): void {
-        firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(usuario.email, usuario.senha)
+    public cadastrarUsuario(usuario: Usuario): Promise<any> {
+        return firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(usuario.email, usuario.senha)
             .then((response) => {
                 console.log(response)
 

735eb5fd1867f5f94973bca0f6718325a2e75eb2
goesdgbp <davidsongoes@gmail.com>
2018-06-04 10:35:22 -0300

    Importando o serviço de autenticação dentro do app.module

--- projetos/app3/src/app/app.module.ts
+++ projetos/app3/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { BannerComponent } from './acesso/banner/banner.component';
 import { LoginComponent } from './acesso/login/login.component';
 import { CadastroComponent } from './acesso/cadastro/cadastro.component';
 
+import { AutenticacaoService } from './autenticacao.service'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +24,7 @@ import { CadastroComponent } from './acesso/cadastro/cadastro.component';
     BrowserAnimationsModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AutenticacaoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

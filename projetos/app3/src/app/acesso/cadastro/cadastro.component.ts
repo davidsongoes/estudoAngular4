a60f67bf373697057b10e8894b96014330ba80bf
goesdgbp <davidsongoes@gmail.com>
2018-06-04 10:26:08 -0300

    Implementando dados do form no objeto Usuario

--- projetos/app3/src/app/acesso/cadastro/cadastro.component.ts
+++ projetos/app3/src/app/acesso/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FormGroup, FormControl, Validator, Validators } from '@angular/forms'
+import {Usuario } from '../usuario.model'
 
 @Component({
   selector: 'app-cadastro',
@@ -28,6 +29,13 @@ export class CadastroComponent implements OnInit {
   }
 
   public cadastrarUsuario(): void {
-    console.log(this.formulario)
+    let usuario: Usuario = new Usuario(
+      this.formulario.value.email,
+      this.formulario.value.nome_completo,
+      this.formulario.value.nome_usuario,
+      this.formulario.value.senha
+    )
+
+    console.log(usuario)
   }
 }

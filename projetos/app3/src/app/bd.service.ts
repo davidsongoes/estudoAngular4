253920a741e9cb1da762a099ad869ff2f220689d
davidsongoes <goesdgbp@fab.mil.br>
2018-06-07 17:04:05 -0300

    Implementando o acompanhamento do processo de upload para o firebase

--- projetos/app3/src/app/bd.service.ts
+++ projetos/app3/src/app/bd.service.ts
@@ -9,6 +9,20 @@ export class BdService {
         firebase.storage().ref()
         .child(`imagens/${nomeImagem}`)
         .put(publicacao.imagem)
+        .on(firebase.storage.TaskEvent.STATE_CHANGED,
+            // Ação para o acompanhamento
+        (snapshot: any) => {
+            console.log(snapshot)
+        },
+        (error) => {
+            console.log(error)
+        },
+        () => {
+            // finalização do upload
+            console.log('Upload completado')
+        }
+        
+        )
         // firebase.database().ref(`publicacao/${btoa(publicacao.email)}`)
         // .push( { titulo: publicacao.titulo} )
         // console.log(publicacao)

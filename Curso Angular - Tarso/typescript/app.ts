b330b45383a3854c427078bb6815cedb0e62b959
davidsongoes <goesdgbp@fab.mil.br>
2018-06-11 16:25:50 -0300

    Iniciando em Typescript

---
+++ Curso Angular - Tarso/typescript/app.ts
@@ -0,0 +1,7 @@
+let message: string = 'Olá'
+console.log(message)
+
+let episode: number = 4
+console.log("This is episode " + 4)
+episode = episode + 1
+console.log("Next episode is " + episode)
---
+++ Curso Angular - Tarso/typescript/dist/app.js
@@ -0,0 +1,6 @@
+var message = 'Olá';
+console.log(message);
+var episode = 4;
+console.log("This is episode " + 4);
+episode = episode + 1;
+console.log("Next episode is " + episode);

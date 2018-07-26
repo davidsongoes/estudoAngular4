7e6d9e39652235702403c6caa623ccaa6181793f
davidsongoes <davidsongoes@gmail.com>
2018-07-26 08:42:40 -0300

    Implementando componente item de menu

--- Curso Angular - Tarso/meat-app-starter-master/src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ Curso Angular - Tarso/meat-app-starter-master/src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { MenuItem } from './menu-item.model'
 
 @Component({
   selector: 'mt-menu-item',
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuItemComponent implements OnInit {
 
+
+  @Input() menuItem: MenuItem
+  @Output() add: EventEmitter<any> = new EventEmitter()
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  emitAddEvent(): void {
+    this.add.emit(this.menuItem)
   }
 
 }

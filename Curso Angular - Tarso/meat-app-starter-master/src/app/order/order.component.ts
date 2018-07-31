b7470adbed972712d689ba566f32f9d1ee1e4a7c
davidsongoes <davidsongoes@gmail.com>
2018-07-31 17:25:17 -0300

    Refatorando para o componente de frete

--- Curso Angular - Tarso/meat-app-starter-master/src/app/order/order.component.ts
+++ Curso Angular - Tarso/meat-app-starter-master/src/app/order/order.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { RadioOption } from '../shared/radio/radio-option.model';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
 import { OrderService } from './order.service';
+import { Order, OrderItem } from './order.model';
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+  delivery: number = 8
 
   paymentOptions: RadioOption[] = [
     {label: 'Dinheiro', value: 'MON'},
@@ -18,6 +21,10 @@ export class OrderComponent implements OnInit {
   constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+  }
+
+  itemsValue(): number {
+    return this.orderService.itemsValue()
   }
 
   cartItems(): CartItem[]{
@@ -34,5 +41,14 @@ export class OrderComponent implements OnInit {
 
   remove(item: CartItem){
     this.orderService.remove(item)
+  }
+
+  checkOrder(order: Order){
+    order.orderItems = this.cartItems().map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
+    this.orderService.checkOrder(order).subscribe((orderId) => {
+    console.log(`Compra Concluída: `, orderId)
+    this.orderService.clear()
+    })
+    console.log(order)
   }
 }

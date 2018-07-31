0d821d456ff1653e33ad5ca0f7fe058a64305e7b
davidsongoes <davidsongoes@gmail.com>
2018-07-31 17:26:03 -0300

    Implementando a persistência da compra no backend

--- Curso Angular - Tarso/meat-app-starter-master/src/app/order/order.service.ts
+++ Curso Angular - Tarso/meat-app-starter-master/src/app/order/order.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from "@angular/core";
 import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
 import { CartItem } from "../restaurant-detail/shopping-cart/cart-item.model";
+import { Order } from "./order.model";
+import { Observable } from 'rxjs/Observable'
+import { Http, Headers, RequestOptions } from '@angular/http'
+import { MEAT_API } from '../app.api'
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class OrderService {
 
-    constructor(private shoppingCartService: ShoppingCartService) { }
+    constructor(private shoppingCartService: ShoppingCartService, private http: Http) { }
 
     cartItems(): CartItem[] {
         return this.shoppingCartService.items
@@ -21,5 +26,20 @@ export class OrderService {
 
     remove(item: CartItem) {
         this.shoppingCartService.removeItem(item)
+    }
+
+    itemsValue(): number {
+        return this.shoppingCartService.total()
+    }
+
+    checkOrder(order: Order): Observable<string> {
+        const headers = new Headers()
+        headers.append('Content-Type', 'application/json')
+        return this.http.post(`${MEAT_API}/orders`, JSON.stringify(order), new RequestOptions({ headers: headers }))
+            .map((response) => response.json())
+    }
+
+    clear(){
+        this.shoppingCartService.clear()
     }
 }

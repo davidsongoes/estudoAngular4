366abf90de9d66360cfb78ca2e6712b3b174b83e
Davidson Góes <davidsongoes@gmail.com>
2018-08-10 10:30:20 -0300

    Implementando validação dos campos de emails

--- Curso Angular - Tarso/meat-app-starter-master/src/app/order/order.component.ts
+++ Curso Angular - Tarso/meat-app-starter-master/src/app/order/order.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms'
 import { Router } from '@angular/router'
 import { RadioOption } from '../shared/radio/radio-option.model';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
@@ -41,7 +41,19 @@ export class OrderComponent implements OnInit {
       number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(''),
       paymentOption: this.formBuilder.control('',[Validators.required])
-    })
+    }, {validator: OrderComponent.equalsTo})
+  }
+
+  static equalsTo(group: AbstractControl): {[key: string]: boolean} {
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+    if(!email || ! emailConfirmation){
+      return undefined  
+    }
+    if(email.value !== emailConfirmation.value){
+      return { emailsNotMatch: true}
+    }
+    return undefined
   }
 
   itemsValue(): number {

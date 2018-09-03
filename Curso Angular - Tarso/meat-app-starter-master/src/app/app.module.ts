fd60fe31b556d6d896dd69dfd4803b3674fd2a93
Davidson Góes <davidsongoes@gmail.com>
2018-09-03 14:56:46 -0300

    Adicionando snackbar

--- Curso Angular - Tarso/meat-app-starter-master/src/app/app.module.ts
+++ Curso Angular - Tarso/meat-app-starter-master/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import { FormsModule } from '@angular/forms'
 import { ReactiveFormsModule } from '@angular/forms'
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 import { ROUTES } from './app.routes'
 
@@ -39,8 +40,9 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpModule,
-    RouterModule.forRoot(ROUTES),
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules}),
     SharedModule.forRoot(),
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],

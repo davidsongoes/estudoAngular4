cb1924499432311bc909df97515876072759bbf4
davidsongoes <davidsongoes@gmail.com>
2018-09-17 13:51:07 -0300

    Criando rota wildcard para not found

--- Curso Angular - Tarso/meat-app-starter-master/src/app/app.routes.ts
+++ Curso Angular - Tarso/meat-app-starter-master/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 import { ShoppingCartComponent } from "./restaurant-detail/shopping-cart/shopping-cart.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
 import { OrderSummaryComponent } from "./order-summary/order-summary.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
@@ -20,6 +21,7 @@ export const ROUTES: Routes = [
             { path: 'menu', component: MenuComponent },
             { path: 'reviews', component: ReviewsComponent }
         ]
-    }
+    },
+    { path: '**', component: NotFoundComponent }
 
 ]

0dd3f627e0859dca0bd156c2279d1ab09feb39c5
davidsongoes <davidsongoes@gmail.com>
2018-09-19 10:24:44 -0300

    Adicionando animação no review

--- Curso Angular - Tarso/meat-app-starter-master/src/app/restaurant-detail/reviews/reviews.component.ts
+++ Curso Angular - Tarso/meat-app-starter-master/src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
 import { RestaurantsService } from '../../restaurants/restaurants.service';
 import { Observable } from 'rxjs/Observable'
 import { ActivatedRoute } from '@angular/router';
+import { style, state, transition, trigger, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-reviews',
-  templateUrl: './reviews.component.html'
+  templateUrl: './reviews.component.html',
+  animations: [
+    trigger('reviewAppeared', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', [
+        style({ opacity: 0, transform: 'translateY(-20px)' }),
+        animate('300ms 0s ease-in')
+      ])
+    ])
+  ]
 })
 export class ReviewsComponent implements OnInit {
 
   reviews: Observable<any>
+  reviewState: string = 'ready'
 
   constructor(
     private restaurantsService: RestaurantsService,
